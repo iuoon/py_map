f2d4fcf82fa31d8040115ab34ca23badc01fe2e7
iuoon <710731936@qq.com>
2019-04-03 21:08:25 +0800

    代码修改

--- main.py
+++ main.py
@@ -10,9 +10,10 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.triggers.cron import CronTrigger
 import threading
 import city
-from openpyxl import Workbook
+from openpyxl import Workbook,load_workbook
 
-APP_TITLE = u'爬取数据'
+
+APP_TITLE = u'爬取数据-爬限速测试版'
 APP_ICON = 'res/python.ico'
 
 class mainFrame(wx.Frame):
@@ -23,7 +24,7 @@ class mainFrame(wx.Frame):
 
         wx.Frame.__init__(self, parent, -1, APP_TITLE)
         self.SetBackgroundColour(wx.Colour(255, 255, 255))
-        self.SetSize((600, 390))
+        self.SetSize((600, 480))
         self.Center()
 
         # if hasattr(sys, "frozen") and getattr(sys, "frozen") == "windows_exe":
@@ -35,10 +36,10 @@ class mainFrame(wx.Frame):
 
         wx.StaticText(self, -1, u'设置高德地图Key：', pos=(10, 20), size=(100, -1), style=wx.ALIGN_RIGHT)
         # tip
-        self.tip = wx.StaticText(self, -1, u'高德地图获取交通态势需要创建应用key，请到官网创建web服务key', pos=(20, 270), size=(400, -1), style=wx.ST_NO_AUTORESIZE)
-        self.tip2 = wx.StaticText(self, -1, u"矩形坐标全用英文逗号隔开，格式为：左下角经纬度,右上角经纬度 ", pos=(20, 290), size=(400, -1), style=wx.ST_NO_AUTORESIZE)
-        self.tip3 = wx.StaticText(self, -1, u"取经纬度坐标地址：https://lbs.amap.com/console/show/picker", pos=(20, 310), size=(400, -1), style=wx.ST_NO_AUTORESIZE)
-        self.tip3 = wx.StaticText(self, -1, u"若设置了经纬度矩形区域，则开始时按矩形抓取，否则按选择城市抓取", pos=(20, 330), size=(400, -1), style=wx.ST_NO_AUTORESIZE)
+        self.tip = wx.StaticText(self, -1, u'高德地图获取交通态势需要创建应用key，请到官网创建web服务key', pos=(20, 340), size=(400, -1), style=wx.ST_NO_AUTORESIZE)
+        self.tip2 = wx.StaticText(self, -1, u"矩形坐标全用英文逗号隔开，格式为：左下角经纬度,右上角经纬度 ", pos=(20, 360), size=(400, -1), style=wx.ST_NO_AUTORESIZE)
+        self.tip3 = wx.StaticText(self, -1, u"取经纬度坐标地址：https://lbs.amap.com/console/show/picker", pos=(20, 380), size=(400, -1), style=wx.ST_NO_AUTORESIZE)
+        self.tip3 = wx.StaticText(self, -1, u"若设置了经纬度矩形区域，则开始时按矩形抓取，否则按选择城市抓取", pos=(20, 400), size=(400, -1), style=wx.ST_NO_AUTORESIZE)
         # key
         self.gd_key = wx.TextCtrl(self, -1, '', pos=(130, 20), size=(200, -1), name='GD_KEY', style=wx.TE_LEFT)
 
@@ -48,6 +49,7 @@ class mainFrame(wx.Frame):
 
         self.btn_start = wx.Button(self, -1, u'开始', pos=(350, 20), size=(100, 25))
         self.btn_pause = wx.Button(self, -1, u'暂停', pos=(350, 50), size=(100, 25))
+
         #self.btn_close = wx.Button(self, -1, u'关闭窗口', pos=(350, 80), size=(100, 25))
         wx.StaticText(self, -1, u'设置矩形坐标点：', pos=(350, 80), size=(100, -1), style=wx.ALIGN_LEFT)
         self.loat = wx.TextCtrl(self, -1, '', pos=(350, 100), size=(200, -1), name='loat', style=wx.TE_LEFT)
@@ -63,6 +65,11 @@ class mainFrame(wx.Frame):
         self.preCity = '-1'  # 上一次城市
         self.cityTip = wx.StaticText(self, -1, u'当前城市：未选择', pos=(350, 230), size=(400, -1), style=wx.ST_NO_AUTORESIZE)
 
+        self.btn_start2 = wx.Button(self, -1, u'开始爬取限速', pos=(20, 280), size=(100, 25))
+        self.btn_cancel2 = wx.Button(self, -1, u'取消爬取限速', pos=(140, 280), size=(100, 25))
+
+        self.btn_cancel2.Disable()
+
         # 控件事件
         #self.gd_key.Bind(wx.EVT_TEXT, self.EvtText)
 
@@ -72,10 +79,14 @@ class mainFrame(wx.Frame):
         self.Bind(wx.EVT_BUTTON, self.OnPauseDown, self.btn_pause)
         self.Bind(wx.EVT_COMBOBOX, self.OnProvinceChoice, self.ch1)
         self.Bind(wx.EVT_COMBOBOX, self.OnCityChoice, self.ch2)
+
+        self.btn_start2.Bind(wx.EVT_LEFT_DOWN, self.StartReptileRoad)
+        self.btn_cancel2.Bind(wx.EVT_LEFT_DOWN, self.CancelReptileRoad)
         # 暂停按钮启动时不可点击
         self.btn_pause.Disable()
         # 创建异步执行爬取数据的线程
         self.t1 = threading.Thread(target=self.startWork, args=(self.gd_key.GetValue(),))
+        self.t2 = threading.Thread(target=self.startWork2, args=(self.gd_key.GetValue(),))
 
         # 系统事件
         self.Bind(wx.EVT_CLOSE, self.OnClose)
@@ -104,7 +115,8 @@ class mainFrame(wx.Frame):
     iswriting=False   # 是否正在开始写xls
     ispause = False   # 是否暂停
     isreptiling = False  #是否在爬取中
-    isrunsched =False #是否开启了调度
+    isrunsched =False  #是否开启了调度
+    ispause2=False     # 是否开启爬取限速
 
     def OnClose(self, evt):
         # 关闭窗口事件函数
@@ -257,7 +269,8 @@ class mainFrame(wx.Frame):
                 self.area.AppendText('[warn]'+str(data)+'\n')
                 continue
 
-            print(data)
+
+            # print(data)
 
             description = data['trafficinfo']['description'] if 'description' in data['trafficinfo'] else ''
             evaluation = data['trafficinfo']['evaluation'] if 'evaluation' in data['trafficinfo'] else ''
@@ -285,9 +298,19 @@ class mainFrame(wx.Frame):
                 sheet1.cell(row=count, column=9).value = evaluation
                 sheet1.cell(row=count, column=10).value = dttime
 
-                # remove_digits = str.maketrans('', '', digits)
-                # res = ss.translate(remove_digits).replace('.,.', '1').replace(';', ',')
+                remove_digits = str.maketrans('', '', digits)
+                s2 = rpolyline.translate(remove_digits).replace('.,.', '1').replace(';', ',')
 
+                print('11polyline=======>', rpolyline)
+                print('22speed=======>', s2)
+                arr = s2.split(",")
+                s3 = ""
+                t = time.time()
+                for i in range(0,len(arr)):
+                    tt=int(t)+i
+                    s3 = s3+str(tt)+','
+                s3 = s3[:len(s3)-1]
+                print ('33time=======>', s3)
                 # param0={
                 #     'key': str(key),
                 #     'extensions': 'all',
@@ -323,6 +346,100 @@ class mainFrame(wx.Frame):
         self.area.AppendText('[info]暂停爬取数据...\n')
         self.ispause=True
         self.scheduler.pause()
+
+
+    scheduler2 = BlockingScheduler()
+    def StartReptileRoad(self, evt):
+        self.btn_start2.Disable()
+        self.btn_cancel2.Enable()
+        key = self.gd_key.GetValue()
+        if key == '':
+            key = '0b1804994cd63974f873a29a269d65e7'
+        self.reptileRoad(key)
+        if self.ispause2:
+            self.ispause2=False
+            self.scheduler2.resume()
+        else:
+            self.t2 = threading.Thread(target=self.startWork2, args=(key,))
+            self.t2.setDaemon(True)  # 设置为守护线程
+            self.t2.start()
+
+    def startWork2(self,key):
+        # 定时每天 00:00:30秒执行任务
+        trigger2 = CronTrigger(day_of_week='0-6', hour = 0,minute = 0,second = 30 )
+        self.scheduler2.add_job(self.reptileRoad, trigger2, args=(key,))
+        self.scheduler2.start()
+
+
+    def CancelReptileRoad(self, evt):
+        self.btn_start2.Enable()
+        self.btn_cancel2.Disable()
+        self.ispause2=True
+        self.scheduler2.pause()
+
+
+    def reptileRoad(self,key):
+        # 获取道路文件路径
+        for root, dirs, files in os.walk(os.path.abspath('.')):
+           dir=time.strftime("%Y%m%d")
+           if root.endswith(dir) and len(files)>0:
+               filePath = root+"\\"+files[0]
+               print('file:', filePath)
+               wb = load_workbook(filePath)
+               sheet = wb.active
+               rnum=sheet.max_row
+               cnum=sheet.max_column
+               for r in range(2, rnum):
+                   polyline =sheet.cell(row=r, column=5).value
+                   s1 = polyline.replace(';', '|') # 点参数
+                   remove_digits = str.maketrans('', '', digits)
+                   s2 = polyline.translate(remove_digits).replace('.,.', '1').replace(';', ',')
+                   arr = s2.split(",")  #方向速度参数
+                   s3 = ""  # 时间戳参数
+                   t = time.time()
+                   for i in range(0,len(arr)):
+                       tt=int(t)+i
+                       s3 = s3+str(tt)+','
+                   s3 = s3[:len(s3)-1]
+                   param={
+                      'key': str(key),
+                      'extensions': 'all',
+                      'carid': 'ts001',
+                      'locations': s1,
+                      'direction': s2,
+                      'speed': s2,
+                      'time': s3
+                   }
+                   obj = '{}'
+                   while True:
+                       try:
+                           obj =requests.get('https://restapi.amap.com/v3/autograsp?', params=param, timeout=30)
+                           break
+                       except requests.exceptions.ConnectionError:
+                           print('[ERROR]ConnectionError2 -- will retry connect')
+                           self.area.AppendText('[ERROR]ConnectionError2 -- will retry connect\n')
+                           time.sleep(1)
+
+                   data = obj.json()
+                   if data['status'] == '0':
+                       print('[info]'+str(data))
+                       print('[warn]请求参数错误',str(param))
+                       continue
+                   if data['status'] == '1':
+                       for road in data['roads']:
+                           maxspeed= road.get("maxspeed")
+                           roadlevel= road.get("roadlevel")
+                           if maxspeed is None or maxspeed =='' or maxspeed == "-1":
+                               continue
+                           else:
+                               sheet.cell(row=r, column=11).value = roadlevel
+                               sheet.cell(row=r, column=12).value = maxspeed
+                               break
+                   if r>10:
+                       break
+               wb.save(filePath)
+               print(filePath, "爬取道路限速和道路等级成功")
+
 
 class mainApp(wx.App):
     def OnInit(self):
