1657d7bcb76bcb89bdf598008a4ddf7c6d941d47
iuoon <1272461442@qq.com>
2019-07-23 23:48:10 +0800

    代码修改

--- qrcode.py
+++ qrcode.py
@@ -1,49 +1,56 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 import pyzbar.pyzbar as pyzbar
-import numpy as np
-from PIL import Image, ImageDraw, ImageFont
 import cv2
+import zxing
+import os
+from openpyxl import Workbook
+
 
 if __name__ == "__main__":
-    frame= cv2.imread('804.jpg')
-    # 转为灰度图像
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    xlsfile = 'output.xlsx'
+    imgs=[]
+    for root, dirs, files in os.walk(os.path.abspath('.')):
+        if len(files)>0:
+            imgs=files
+            break
+    if len(imgs)== 0:
+        print("目录下没有文件")
+    else:
+        wb = Workbook()    #创建文件对象
+        ws = wb.active
+        ws.append(["图片名", "序号（左上角）", "成绩（右上角）"])
+        dd=root.replace("\\", "/")
+        dd="file:/"+dd+"/tmp/"
+        if not os.path.exists(root+"\\tmp\\"):
+            os.makedirs(dd)
+        for img in imgs:
+            if img.endswith('.jpg') or img.endswith('.png'):
+                 print("开始解析文件："+img+"\n")
+                 frame= cv2.imread(img)
+                 cropped = frame[0:290, 100:512]  # 裁剪坐标为[y0:y1, x0:x1]
+                 cv2.imwrite(root+"\\tmp\\"+img, cropped)
 
-    barcodes = pyzbar.decode(gray)
-    print(barcodes)
-    for barcode in barcodes:
-        # 提取条形码的边界框的位置
-        # 画出图像中条形码的边界框
-        (x, y, w, h) = barcode.rect
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 255, 0), 2)
+                 #x, y = frame.shape[0:2]
+                 #frame = cv2.resize(frame, (int(y * 2), int(x * 2)))
+                 reader1 = zxing.BarCodeReader()
+                 barcode1 = reader1.decode(dd+img)
+                 print(barcode1)
+                 print(img+"解析出二维码1数值："+barcode1.parsed)
+                 # 转为灰度图像
+                 gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        # 条形码数据为字节对象，所以如果我们想在输出图像上
-        #  画出来，就需要先将它转换成字符串
-        barcodeData = barcode.data.decode("utf-8")
-        # 绘出图像上条形码的数据和条形码类型
-        barcodeType = barcode.type
+                 barcodes = pyzbar.decode(gray)
+                 barcodeData=''
+                 if len(barcodes)>0:
+                     barcode=barcodes[0]
+                     barcodeData = barcode.data.decode("utf-8")
+                     print(img+"解析出二维码2数值："+barcodeData)
+                 data=[img,barcode1.parsed,barcodeData]
+                 ws.append(data)
+        wb.save(xlsfile)
 
-        # 把cv2格式的图片转成PIL格式的图片然后在上标注二维码和条形码的内容
-        img_PIL = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
 
-        # 参数（字体，默认大小）
-        font = ImageFont.truetype('simsunb.ttf', 25)
 
-        # 字体颜色
-        fillColor = (0,255,0)
 
-        # 文字输出位置
-        position = (x, y-25)
 
-        # 输出内容
-        strl = barcodeData
-        # 需要先把输出的中文字符转换成Unicode编码形式(str.decode("utf-8))
-
-        # 创建画笔
-        draw = ImageDraw.Draw(img_PIL)
-        draw.text(position, strl, font=font,fill=fillColor)
-        # 使用PIL中的save方法保存图片到本地
-        #  img_PIL.save('结果图3.jpg', 'jpeg')
-        # 向终端打印条形码数据和条形码类型
-        print("扫描结果==》 类别： {0} 内容： {1}".format(barcodeType, barcodeData))
