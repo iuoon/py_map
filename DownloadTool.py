e3ef021d8a5689d34a5fba9d2a057f5dfc7b2fe0
iuoon <mhwuyz@163.com>
2021-06-07 15:43:53 +0800

    下载文件修改

--- DownloadTool.py
+++ DownloadTool.py
@@ -1,9 +1,11 @@
 import wx
 import requests
-import time
 import threading
+import os
+from urllib.request import urlretrieve
+from openpyxl import load_workbook
 
-APP_TITLE = u'身份认证'
+APP_TITLE = u'下载文件工具'
 APP_ICON = 'res/python.ico'
 
 
@@ -31,205 +33,93 @@ class mainFrame(wx.Frame):
 
         self.btn_start = wx.Button(self, -1, u'开始下载', pos=(10, 160), size=(80, -1))
 
+        self.Bind(wx.EVT_BUTTON, self.OnSelectExcel, self.selectExcelBtn)
+        self.Bind(wx.EVT_BUTTON, self.OnSelectOutPath, self.selectOutPathBtn)
         self.btn_start.Bind(wx.EVT_LEFT_DOWN, self.startWork)
 
-        self.currentStep = ''
-        self.entityName = ''
+    def OnSelectExcel(self, event):
+        dlg = wx.FileDialog(self,message=u"选择文件",
+                            defaultDir=os.getcwd(),
+                            defaultFile="")
+        if dlg.ShowModal() == wx.ID_OK:
+            print(dlg.GetPath())  # 文件夹路径
+            self.excelFile.SetLabelText(dlg.GetPath())
+        dlg.Destroy()
+
+    def OnSelectOutPath(self, event):
+        dlg = wx.DirDialog(self, u"选择文件夹", style=wx.DD_DEFAULT_STYLE)
+        if dlg.ShowModal() == wx.ID_OK:
+            print(dlg.GetPath())  # 文件夹路径
+            self.outPath.SetLabelText(dlg.GetPath())
+        dlg.Destroy()
 
     def startWork(self, event):
-        if self.host.GetValue() == '':
-            self.area.AppendText("请输入host\n")
+        if self.excelFile.GetValue() == '':
+            self.area.AppendText("请选择Excel文件\n")
             return
-        if self.host.GetValue() == '':
-            self.area.AppendText("请输入cookie\n")
-            return
-        if self.currentStep == '':
-            self.area.AppendText("请选择类型\n")
+        if self.outPath.GetValue() == '':
+            self.area.AppendText("请选择输出文件夹\n")
             return
 
-        t1 = threading.Thread(target=self.preReadCard, )
+        t1 = threading.Thread(target=self.pre_work)
         t1.start()
 
-    def preReadCard(self):
-        self.area.AppendText("开始认证\n")
-        self.host.Disable()
+    def pre_work(self):
+
+        self.excelFile.Disable()
+        self.outPath.Disable()
         self.cookie.Disable()
         self.btn_start.Disable()
-        self.ch1.Disable()
-        for line in open("card.txt", "r", encoding="utf-8"):  # 设置文件对象并读取每一行文件
-            arr = line.split("#")
-            print(arr[1])
-            print(arr[2])
-            name = arr[1].replace("\n", '')
-            cardNo = arr[2].replace("\n", '')
-            if self.readCard(name, cardNo) == False:
-                print("认证方式一失败，开始尝试第二种方式")
-                self.area.AppendText(name + cardNo + "认证方式一失败，开始尝试第二种方式\n")
-                if self.readCard2(name, cardNo) == False:
-                    self.area.AppendText(name + cardNo + "认证失败,请更换cookie重试\n")
-                else:
-                    self.area.AppendText(name + cardNo + "认证成功\n")
-            else:
-                self.area.AppendText(name + cardNo + "认证成功\n")
-            time.sleep(10)
+        self.area.AppendText("开始加载企业列表\n")
+        ent_list = self.read_excel()
+        size = len(ent_list)
+        if size > 0 or size < 0:
+            self.area.AppendText("加载企业失败,结束下载\n")
+            return
+
+        self.area.AppendText("加载完毕，开始下载文件\n")
+        for r in range(3, ):
+            ent_name = ent_list.pop(r)
+            self.download(ent_name)
+
         self.area.AppendText("认证结束\n")
-        self.host.Enable()
+        self.excelFile.Enable()
+        self.outPath.Enable()
         self.cookie.Enable()
         self.btn_start.Enable()
-        self.ch1.Enable()
 
-    def readCard(self, name, cardNo):
+    def read_excel(self):
+        ent_list = []
+        print('excel：', self.excelFile.GetValue())
+        wb = load_workbook(self.excelFile.GetValue())
+        sheet = wb.active
+        rnum = sheet.max_row
+        cnum = sheet.max_column
+        for r in range(3, rnum):
+            ent_name = sheet.cell(row=r, column=3).value
+            print(ent_name)
+            if ent_name is None:
+                continue
+            ent_list.append(ent_name)
+        return ent_list
 
-        header = {'Accept': 'application/json, text/javascript, */*; q=0.01',
-                  'Referer': self.host.GetValue() + '/E-office/sit/employeeReadCard',
-                  'x-requested-with': 'XMLHttpRequest',
-                  'Accept-Encoding': 'gzip, deflate',
-                  'Accept-Language': 'zh-CN',
-                  'Cache-Control': 'max-age=0',
-                  'Connection': 'keep-alive',
-                  'Accept-Encoding': 'gzip, deflate',
-                  # 'Host': '222.82.237.75:8070',
-                  'Pragma': 'no-cache',
-                  'Content-Type': 'application/x-www-form-urlencoded',
-                  'User-Agent': 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; InfoPath.3; .NET4.0C)',
-                  'Cookie': self.cookie.GetValue()
-                  }
-        paramData = {
-            "pkId": self.pkId.GetValue(),  # 27694
-            "cardNo": cardNo,
-            "entityName": self.entityName
-        }
-        ret = '{"status":False}'
-        data = '{}'
-        while True:
-            try:
-                ret = requests.post(self.host.GetValue() + '/E-office/sit/getEmployeeByIdCard', data=paramData,
-                                    timeout=30, headers=header)
-                print(ret.text)
-                break
-            except requests.exceptions.ConnectionError:
-                print('[ERROR]ConnectionError -- will retry connect')
-                time.sleep(3)
-        try:
-            data = ret.json()
-        except:
-            return False
-        try:
-            if data['status']:
-                employeeData = data['data'][0]
-                idCardPhoto = ''
-                employeeId = '',
-                for de in employeeData:
-                    if de['field'] == 'employeeId':
-                        employeeId = de['value']
-                        idCardPhoto = de['idCardPhoto']
-                        break
-                print(idCardPhoto)
-                print(str(employeeId))
-                pa = {
-                    "pkId": self.pkId.GetValue(),
-                    "idCard": cardNo,
-                    "idCardPhoto": str(idCardPhoto),
-                    "idCardName": name,
-                    "employeeId": employeeId,
-                    "currentStep": self.currentStep,
-                    "confirmStatus": "Y",
-                    "sync": "false"
-                }
-                while True:
-                    try:
-                        obj = requests.post(self.host.GetValue() + '/E-office/sit/employeeIdConfirm', data=pa,
-                                            timeout=30, headers=header)
-                        try:
-                            dt1 = obj.json()
-                            if dt1['status'] == False:
-                                return False
-                        except:
-                            return False
-                        break
-                    except requests.exceptions.ConnectionError:
-                        print('[ERROR]ConnectionError -- will retry connect')
-                        time.sleep(3)
-            else:
-                return False
-        except:
-            return False
+    def download(self, ent_name):
+        down_url = "http://www.baidu.com/" + ent_name + ".pdf"
+        self.download_file1(down_url, self.outPath)
         return True
 
-    def readCard2(self, name, card):
-        header = {'Accept': 'application/json, text/javascript, */*; q=0.01',
-                  'Referer': self.host.GetValue() + '/E-office/sit/employeeReadCard',
-                  'x-requested-with': 'XMLHttpRequest',
-                  'Accept-Encoding': 'gzip, deflate',
-                  'Accept-Language': 'zh-CN',
-                  'Cache-Control': 'max-age=0',
-                  'Connection': 'keep-alive',
-                  'Accept-Encoding': 'gzip, deflate',
-                  'Host': '222.82.237.75:8070',
-                  'Pragma': 'no-cache',
-                  'Content-Type': 'application/x-www-form-urlencoded',
-                  'User-Agent': 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; InfoPath.3; .NET4.0C)',
-                  'Cookie': self.cookie.GetValue()
-                  }
-        paramData = {
-            "licenseNum": self.licenseNum.GetValue(),
-            "cardNo": str(card),
-            "employeeName": str(name),
-            "entityName": self.entityName
-        }
-        ret = '{"status":False}'
-        data = '{}'
-        while True:
-            try:
-                ret = requests.post(self.host.GetValue() + '/E-office/sit/getEmployeeByIdCard', data=paramData,
-                                    timeout=30, headers=header)
-                print(ret.text)
-                break
-            except requests.exceptions.ConnectionError:
-                print('[ERROR]ConnectionError -- will retry connect')
-                time.sleep(3)
-        try:
-            data = ret.json()
-        except:
-            return False
-        if data['status']:
-            employeeData = data['data'][0]
-            idCardPhoto = ''
-            employeeId = '',
-            for de in employeeData:
-                if de['field'] == 'employeeId':
-                    employeeId = de['value']
-                    idCardPhoto = de['idCardPhoto']
-                    break
-            pa = {
-                "licenseNum": self.licenseNum.GetValue(),
-                "idCard": card,
-                "idCardPhoto": str(idCardPhoto),
-                "idCardName": name,
-                "employeeId": str(employeeId),
-                "currentStep": self.currentStep,
-                "confirmStatus": "Y",
-                "sync": "false"
-            }
-            print(pa)
-            while True:
-                try:
-                    obj = requests.post(self.host.GetValue() + '/E-office/sit/employeeIdConfirm', data=pa, timeout=30,
-                                        headers=header)
-                    try:
-                        dt1 = obj.json()
-                        print(obj.text)
-                        if dt1['status'] == False:
-                            return False
-                    except:
-                        return False
-                    break
-                except requests.exceptions.ConnectionError:
-                    print('[ERROR]ConnectionError -- will retry connect')
-                    time.sleep(3)
-        else:
-            return False
-        return True
+    def download_file1(self, url, store_path):
+        filename = url.split("/")[-1]
+        filepath = os.path.join(store_path, filename)
+
+        file_data = requests.get(url, allow_redirects=True).content
+        with open(filepath, 'wb') as handler:
+            handler.write(file_data)
+
+    def download_file2(self, url, store_path):
+        filename = url.split("/")[-1]
+        filepath = os.path.join(store_path, filename)
+        urlretrieve(url, filepath)
 
 
 class mainApp(wx.App):
